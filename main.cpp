a54651d69ce95cfb949bd4f9787f523212b92c51
Diogo Santos <97601965+diogosec@users.noreply.github.com>
2023-01-03 17:49:00 +0000

    poo

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using std::cout,std::endl,std::cin;
+
+#include "Banco.h"
+#include "Pessoa.h"
+#include "PessoaFisica.h"
+#include "PessoaJuridica.h"
+#include "ContaComum.h"
+#include "ContaPoupanca.h"
+#include "ContaCorrente.h"
+
+int main() {
+  Banco test;
+  test.iniciar_projeto();
+     
+
+
+}
